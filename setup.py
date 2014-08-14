3ebc3f498ddc84e23e87b93dc18819c9a62cd5b2
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2014-08-14 17:59:03 +0200

    increase verion to 1.3dev7

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_file_contents(file_path):
 
 
 setup(name='privacyideaadm',
-      version='1.3dev6',
+      version='1.3dev7',
       description='privacyIDEA admin Client',
       author='Cornelius Kölbel',
       author_email='cornelius@privacyidea.org',
