229b0fa4798ffa2fcd1eaa37758e1ad31de90196
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2014-08-15 13:59:30 +0200

    fix clear slot bug

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_file_contents(file_path):
 
 
 setup(name='privacyideaadm',
-      version='1.3dev8',
+      version='1.3dev9',
       description='privacyIDEA admin Client',
       author='Cornelius Kölbel',
       author_email='cornelius@privacyidea.org',
